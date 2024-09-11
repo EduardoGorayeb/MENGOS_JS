df791baf55571933a508ed8f0e4767f8e3ff4fb8
Eduardo <eduardomgorayeb@gmail.com>
2024-09-11 16:44:25 -0300

    Atualizações

---
+++ main.js
@@ -0,0 +1,16 @@
+function confirm(){
+    alert('Confirmado!')
+}
+function cancel(){
+    alert('Cancelado!')
+}
+function clear(){
+    alert('Limpo!')
+}
+function img(){
+    alert('Está é uma imagem referente e linguagem de programação JavaScript.')
+}
+function print(){
+    document.print()   
+}
+document.write("<p class='paragraph'>" + document.title + "</p>")
